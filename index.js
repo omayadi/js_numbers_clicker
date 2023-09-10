b7fc4629cc68ffc5c6a5e05a31b39266d9e3d945
omayadi <144386702+omayadi@users.noreply.github.com>
2023-09-10 15:22:12 +0200

    upploded JS app clickNumbers 1.0

---
+++ index.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+let screenEl = document.getElementById("screen-el")
+console.log(number.textContent)
+
+function click1() {
+    screenEl.textContent = 1
+}
+
+function click2() {
+    screenEl.textContent = 2
+}
+
+function click3() {
+    screenEl.textContent = 3
+}
+
+function click4() {
+    screenEl.textContent = 4
+}
+
+function click5() {
+    screenEl.textContent = 5
+}
+
+function click6() {
+    screenEl.textContent = 6
+}
+
+function click7() {
+    screenEl.textContent = 7
+}
+
+function click8() {
+    screenEl.textContent = 8
+}
+
+function click9() {
+    screenEl.textContent = 9
+}
